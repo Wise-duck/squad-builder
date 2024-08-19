e131aaba8fe30cbfa204e0b96ad4295867eab662
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-08-19 17:18:07 +0900

    메뉴 아이템 클릭 시 해당 프래그먼트로 전환하는 기능 구현
    - 앱 첫 실행 시 기본 화면은 축구 포메이션 생성 화면(추후 변경 가능)

--- app/src/main/java/com/example/squadbuilder/MainActivity.kt
+++ app/src/main/java/com/example/squadbuilder/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.squadbuilder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.squadbuilder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.bottom_layout, SoccerFragment()).commit()
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        // 앱 실행 시 축구 포메이션 설정 화면을 기본 화면으로 설정
+        binding.bottomNavigationView.selectedItemId = R.id.main_bottom_nav_soccer
+
+        // 네비게이션 바 아이템 선택 시 화면 전환 설정
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.main_bottom_nav_soccer -> {
+                    replaceFragment(SoccerFragment())
+                    true
+                }
+                R.id.main_bottom_nav_football -> {
+                    replaceFragment(FootballFragment())
+                    true
+                }
+                R.id.main_bottom_nav_list -> {
+                    replaceFragment(ListFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: androidx.fragment.app.Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.bottom_layout, fragment)
+            .commit()
     }
 }
