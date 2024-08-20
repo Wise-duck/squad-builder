0ad5e2de9a4e6d25ebda8ee612ea11b150ad9381
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-08-20 16:04:45 +0900

    뷰모델 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,9 @@ dependencies {
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
 
+    implementation("androidx.fragment:fragment-ktx:1.8.2")  // Fragment KTX (viewModels 사용)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")  // ViewModel KTX
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
