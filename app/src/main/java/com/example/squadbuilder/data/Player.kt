6efc4cf3bb2195d8b887213b92a55d673013fe90
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-08-20 16:05:26 +0900

    플레이어 엔티티 변경
    - 포지션 타입을 문자열로 변경

--- app/src/main/java/com/example/squadbuilder/data/Player.kt
+++ app/src/main/java/com/example/squadbuilder/data/Player.kt
@@ -4,6 +4,7 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
+
 @Entity(
     tableName = "players",
     foreignKeys = [
@@ -18,21 +19,14 @@ import androidx.room.PrimaryKey
 )
 data class Player(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val formationId: Int,  // Formation의 ID 참조
-    val name: String,  // 플레이어 이름
-    val number: Int,  // 플레이어 등 번호
+    var formationId: Int,  // Formation의 ID 참조
+    var name: String,  // 플레이어 이름
+    var number: Int,  // 플레이어 등 번호
+    var position: String,  // 플레이어 포지션
     var x: Float,  // 플레이어 위치(x 좌표 값)
     var y: Float,  // 플레이어 위치(y 좌표 값)
-    val photoUri: String,  // 플레이어 얼굴 사진 설정 기능 구현 시 사용 예정
-    val soccerPosition: SoccerPosition?,  // 풋살 포메이션 생성 시 이 값은 null
-    val footballPosition: FootballPosition?   // 축구 포메이션 생성 시 이 값은 null
+    var photoUri: String,  // 플레이어 얼굴 사진 설정 기능 구현 시 사용 예
 )
 
-enum class SoccerPosition {
-    // 수정 필요
-    GOALKEEPER, DEFENDER, MIDFIELDER, FORWARD
-}
 
-enum class FootballPosition {
-    // 포지션 추가 필요
-}
+
