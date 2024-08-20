7a1ecc7ecd0bc47431af3d3ee6cbe9aca0b55708
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-08-20 22:45:08 +0900

    플레이어 화면 출력을 위한 레이아웃 생성 및 드래그 기능 추가
    - 플레이어 이름, 등 번호, 포지션을 포함하는 레이아웃을 데이터바인딩을 적용해 작성하고 축구 프래그먼트에서 플레이어를 출력할 때 사용함
    - 축구 프래그먼트에서 플레이어를 드래그하여 이동하는 기능을 구현함

--- app/src/main/java/com/example/squadbuilder/fragment/SoccerFragment.kt
+++ app/src/main/java/com/example/squadbuilder/fragment/SoccerFragment.kt
@@ -1,25 +1,29 @@
 package com.example.squadbuilder.fragment
 
+import android.content.ClipData
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
+import android.view.DragEvent
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import android.widget.FrameLayout
-import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.squadbuilder.R
 import com.example.squadbuilder.data.Player
 import com.example.squadbuilder.databinding.FragmentSoccerBinding
+import com.example.squadbuilder.databinding.PlayerViewBinding
 import com.example.squadbuilder.viewmodel.PlayerViewModel
 
 class SoccerFragment : Fragment() {
 
-    private lateinit var binding: FragmentSoccerBinding
+    private lateinit var fragmentBinding: FragmentSoccerBinding
     private val playerViewModel: PlayerViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -28,76 +32,85 @@ class SoccerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSoccerBinding.inflate(inflater, container, false)
+        fragmentBinding = FragmentSoccerBinding.inflate(inflater, container, false)
 
-        // ViewModel을 사용하여 플레이어 리스트를 관찰
-        playerViewModel.players.observe(viewLifecycleOwner, Observer { players ->
-            if (!players.isNullOrEmpty()) {
-                binding.soccerFieldLayout.removeAllViews()  // 기존 플레이어 제거
-                val fieldWidth = binding.soccerFieldLayout.width
-                val fieldHeight = binding.soccerFieldLayout.height
+        fragmentBinding.soccerFieldLayout.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                fragmentBinding.soccerFieldLayout.viewTreeObserver.removeOnGlobalLayoutListener(this)
 
-                Log.d("PLAYER", players.toString())
-                players.forEach { player ->
-                    addPlayerToField(player, fieldWidth, fieldHeight)
-                }
+                val fieldWidth = fragmentBinding.soccerFieldLayout.width
+                val fieldHeight = fragmentBinding.soccerFieldLayout.height
+
+                // ViewModel을 사용하여 플레이어 리스트를 관찰
+                playerViewModel.players.observe(viewLifecycleOwner, Observer { players ->
+                    if (!players.isNullOrEmpty()) {
+                        fragmentBinding.soccerFieldLayout.removeAllViews()  // 기존 플레이어 제거
+
+                        players.forEach { player ->
+                            addPlayerToField(player, fieldWidth, fieldHeight)
+                        }
+                    }
+                })
             }
         })
 
-//        val view = inflater.inflate(R.layout.fragment_soccer, container, false)
-//        val soccerField = view.findViewById<FrameLayout>(R.id.soccerFieldLayout)
-//
-//        // 리스너를 명시적으로 생성
-//        val layoutListener = object : ViewTreeObserver.OnGlobalLayoutListener {
-//            override fun onGlobalLayout() {
-//                // 뷰의 크기가 결정된 후 실행
-//                soccerField.viewTreeObserver.removeOnGlobalLayoutListener(this) // 리스너 제거
-//
-//                // 4-3-3 포메이션 좌표 설정 (x, y 비율)
-//                val positions = arrayOf(
-//                    // 수비 라인 (4명)
-//                    Pair(0.1f, 0.8f), Pair(0.3f, 0.8f), Pair(0.7f, 0.8f), Pair(0.9f, 0.8f),
-//                    // 미드필더 라인 (3명)
-//                    Pair(0.2f, 0.5f), Pair(0.5f, 0.5f), Pair(0.8f, 0.5f),
-//                    // 공격 라인 (3명)
-//                    Pair(0.2f, 0.2f), Pair(0.5f, 0.2f), Pair(0.8f, 0.2f),
-//                    // 골키퍼 (1명)
-//                    Pair(0.5f, 0.9f)
-//                )
-//
-//                val fieldWidth = soccerField.width
-//                val fieldHeight = soccerField.height
-//
-//                for (i in positions.indices) {
-//                    val player = ImageView(context)
-//                    player.layoutParams = FrameLayout.LayoutParams(100, 100).apply {
-//                        leftMargin = (positions[i].first * fieldWidth).toInt() - 50 // 중심 조정을 위해 -50
-//                        topMargin = (positions[i].second * fieldHeight).toInt() - 50 // 중심 조정을 위해 -50
-//                    }
-//                    player.setImageResource(R.drawable.player_shape) // 원 모양 배경 이미지
-//                    soccerField.addView(player)
-//                }
-//            }
-//        }
+        // 드래그 이벤트를 처리하기 위한 리스너 설정
+        fragmentBinding.soccerFieldLayout.setOnDragListener { v, event ->
+            when (event.action) {
+                DragEvent.ACTION_DRAG_STARTED -> true
+                DragEvent.ACTION_DRAG_LOCATION -> {
+                    val view = event.localState as View
+                    view.x = event.x - view.width / 2
+                    view.y = event.y - view.height / 2
+                    view.invalidate()
+                    true
+                }
+                DragEvent.ACTION_DROP -> {
+                    val view = event.localState as View
+                    view.x = event.x - view.width / 2
+                    view.y = event.y - view.height / 2
+                    view.invalidate()
+                    true
+                }
+                DragEvent.ACTION_DRAG_ENDED -> true
+                else -> false
+            }
+        }
 
-        // 리스너 등록
-//        soccerField.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
-
-//        return view
-        return binding.root
+        return fragmentBinding.root
     }
 
-
     private fun addPlayerToField(player: Player, fieldWidth: Int, fieldHeight: Int) {
-        val playerView = ImageView(requireContext())
+        val inflater = LayoutInflater.from(context)
+        val playerBinding: PlayerViewBinding = DataBindingUtil.inflate(inflater, R.layout.player_view, fragmentBinding.soccerFieldLayout, false)
+
+        // 바인딩 객체에 플레이어 데이터 설정
+        playerBinding.player = player
+
+        // 실제 좌표 값 계산
         val x = (player.x * fieldWidth).toInt()
         val y = (player.y * fieldHeight).toInt()
 
-        playerView.layoutParams = FrameLayout.LayoutParams(100, 100).apply {
-            leftMargin = x - 50 // 중심 조정을 위해 -50
-            topMargin = y - 50 // 중심 조정을 위해 -50
+        playerBinding.root.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT).apply {
+            leftMargin = x - (playerBinding.root.measuredWidth / 2)
+            topMargin = y - (playerBinding.root.measuredHeight / 2)
         }
-        playerView.setImageResource(R.drawable.player_shape) // 플레이어 이미지 설정
-        binding.soccerFieldLayout.addView(playerView)
+
+        // 드래그 앤 드롭 설정
+        playerBinding.root.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    val clipData = ClipData.newPlainText("", "")
+                    val shadowBuilder = View.DragShadowBuilder(v)
+                    v.startDragAndDrop(clipData, shadowBuilder, v, 0)
+                    v.performClick() // performClick 호출하여 클릭 이벤트 처리
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // playerView를 필드에 추가
+        fragmentBinding.soccerFieldLayout.addView(playerBinding.root)
     }
 }
