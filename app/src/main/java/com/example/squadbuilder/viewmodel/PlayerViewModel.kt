2847de9c0e24c628ccf2e4cb6c978213b3cc3579
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-08-20 22:37:13 +0900

    플레이어 초기 위치 수정
    - 추후 배경 디자인 수정 시 재수정 필요

--- app/src/main/java/com/example/squadbuilder/viewmodel/PlayerViewModel.kt
+++ app/src/main/java/com/example/squadbuilder/viewmodel/PlayerViewModel.kt
@@ -20,26 +20,29 @@ class PlayerViewModel(application: Application) : AndroidViewModel(application) 
         players = MutableLiveData(createInitialFormation())
     }
 
-    // 수정 필요
+    // 수정 필요(추후 배경 수정 시, 그에 맞는 위치로 조정)
     private fun createInitialFormation(): List<Player> {
         return listOf(
-            Player(formationId = 0, name = "", number = 0, x = 200f, y = 600f, photoUri = "", position = ""),
+            // 1명의 골키퍼
+            Player(formationId = 0, name = "Player", number = 1, x = 0.45f, y = 0.8f, photoUri = "", position = "GK"), // 골키퍼
+
             // 4명의 수비수
-            Player(formationId = 0, name = "", number = 0, x = 100f, y = 400f, photoUri = "", position = ""),
-            Player(formationId = 0, name = "", number = 0, x = 300f, y = 400f, photoUri = "", position = ""),
-            Player(formationId = 0, name = "", number = 0, x = 500f, y = 400f, photoUri = "", position = ""),
-            Player(formationId = 0, name = "", number = 0, x = 700f, y = 400f, photoUri = "", position = ""),
+            Player(formationId = 0, name = "Player", number = 2, x = 0.1f, y = 0.6f, photoUri = "", position = "LB"), // 왼쪽 풀백
+            Player(formationId = 0, name = "Player", number = 3, x = 0.3f, y = 0.6f, photoUri = "", position = "CB"), // 센터백
+            Player(formationId = 0, name = "Player", number = 4, x = 0.5f, y = 0.6f, photoUri = "", position = "CB"), // 센터백
+            Player(formationId = 0, name = "Player", number = 5, x = 0.7f, y = 0.6f, photoUri = "", position = "RB"), // 오른쪽 풀백
+
             // 3명의 미드필더
-            Player(formationId = 0, name = "", number = 0, x = 200f, y = 200f, photoUri = "", position = ""),
-            Player(formationId = 0, name = "", number = 0, x = 400f, y = 200f, photoUri = "", position = ""),
-            Player(formationId = 0, name = "", number = 0, x = 600f, y = 200f, photoUri = "", position = ""),
+            Player(formationId = 0, name = "Player", number = 6, x = 0.2f, y = 0.4f, photoUri = "", position = "LM"), // 왼쪽 미드필더
+            Player(formationId = 0, name = "Player", number = 7, x = 0.4f, y = 0.4f, photoUri = "", position = "CM"),  // 중앙 미드필더
+            Player(formationId = 0, name = "Player", number = 8, x = 0.6f, y = 0.4f, photoUri = "", position = "RM"), // 오른쪽 미드필더
+
             // 3명의 공격수
-            Player(formationId = 0, name = "", number = 0, x = 200f, y = 100f, photoUri = "", position = ""),
-            Player(formationId = 0, name = "", number = 0, x = 400f, y = 100f, photoUri = "", position = ""),
-            Player(formationId = 0, name = "", number = 0, x = 600f, y = 100f, photoUri = "", position = "")
+            Player(formationId = 0, name = "Player", number = 9, x = 0.2f, y = 0.2f, photoUri = "", position = "LW"), // 왼쪽 윙어
+            Player(formationId = 0, name = "Player", number = 10, x = 0.4f, y = 0.2f, photoUri = "", position = "ST"), // 스트라이커
+            Player(formationId = 0, name = "Player", number = 11, x = 0.6f, y = 0.2f, photoUri = "", position = "RW")  // 오른쪽 윙어
         )
     }
-
     fun resetFormation() {
         (players as MutableLiveData).value = createInitialFormation()
     }
