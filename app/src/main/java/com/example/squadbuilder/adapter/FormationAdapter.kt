295eff2237a46a41f0024f69bd4ed22cf70b164a
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-08-21 14:59:31 +0900

    리스트 프래그먼트에 포메이션 정보 출력하는 기능 구현
    - 각 포메이션 정보(팀 이름, 생성 날짜)를 표시할 아이템 레이아웃 formation_list_item.xml 작성
    - 아이템 레이아웃을 사용하여 Formation 객체를 리사이클러뷰의 각 아이템에 바인딩하는 역할을 하는 FormationAdapter.kt 작성
    - 리스트 프래그먼트에서 리사이클러뷰를 설정하고 어댑터를 연결함(뷰모델을 통해 포메이션 리스트를 가져와 어댑터에 전달)

---
+++ app/src/main/java/com/example/squadbuilder/adapter/FormationAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.squadbuilder.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.squadbuilder.R
+import com.example.squadbuilder.data.Formation
+import com.example.squadbuilder.databinding.FormationListItemBinding
+
+class FormationAdapter(private val formations: List<Formation>) : RecyclerView.Adapter<FormationAdapter.FormationViewHolder>() {
+
+    inner class FormationViewHolder(val binding: FormationListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(formation: Formation) {
+            binding.formation = formation
+            binding.executePendingBindings() // 즉시 바인딩 실행
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FormationViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: FormationListItemBinding = DataBindingUtil.inflate(layoutInflater, R.layout.formation_list_item, parent, false)
+        return FormationViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FormationViewHolder, position: Int) {
+        val formation = formations[position]
+        holder.bind(formation)
+    }
+
+    override fun getItemCount(): Int {
+        return formations.size
+    }
+}
--- app/src/main/java/com/example/squadbuilder/fragment/ListFragment.kt
+++ app/src/main/java/com/example/squadbuilder/fragment/ListFragment.kt
@@ -5,18 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.squadbuilder.R
+import com.example.squadbuilder.adapter.FormationAdapter
+import com.example.squadbuilder.databinding.FragmentListBinding
+import com.example.squadbuilder.viewmodel.PlayerViewModel
 
 class ListFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    private lateinit var binding: FragmentListBinding
+    private val playerViewModel: PlayerViewModel by viewModels()
+    private lateinit var formationAdapter: FormationAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        // 데이터 바인딩 초기화
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner // LiveData를 사용할 때 필요
+
+        // ViewModel 설정
+        binding.viewModel = playerViewModel
+
+        // RecyclerView 설정
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        playerViewModel.formationsWithPlayers.observe(viewLifecycleOwner, Observer { formations ->
+            formations?.let {
+                formationAdapter = FormationAdapter(it.map { it.formation })
+                binding.recyclerView.adapter = formationAdapter
+            }
+        })
+
+        return binding.root
     }
 }
