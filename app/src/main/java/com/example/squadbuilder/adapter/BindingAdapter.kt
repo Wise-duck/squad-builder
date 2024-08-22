b32a32e1d16643b40c1f26dbf2ac5d271dfdd5d5
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-08-22 14:23:25 +0900

    리스트 프래그먼트에 출력되는 각각의 포메이션에 팀 사진 출력하는 기능 추가
    - 이미지 로딩을 위한 BindingAdapter를 추가함
    - Glide를 사용해 ImageView에 이미지 URI를 로드하고 기본 이미지를 설정함

---
+++ app/src/main/java/com/example/squadbuilder/adapter/BindingAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.squadbuilder.adapter
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.example.squadbuilder.R
+
+@BindingAdapter("imageUri")
+fun bindImage(imageView: ImageView, imageUri: String?) {
+    if (!imageUri.isNullOrEmpty()) {
+        Glide.with(imageView.context)
+            .load(imageUri)
+            .into(imageView)
+    } else {
+        imageView.setImageResource(R.drawable.ic_launcher_foreground) // 기본 이미지 설정
+    }
+}
