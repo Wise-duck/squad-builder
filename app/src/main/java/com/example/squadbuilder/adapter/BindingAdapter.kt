6d3339dcf59c13619bc8df5998c3ce9a5b851fa5
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-08-23 14:25:50 +0900

    팀 사진 저장하지 않은 경우 기본 이미지 출력되도록 수정

--- app/src/main/java/com/example/squadbuilder/adapter/BindingAdapter.kt
+++ app/src/main/java/com/example/squadbuilder/adapter/BindingAdapter.kt
@@ -10,6 +10,8 @@ fun bindImage(imageView: ImageView, imageUri: String?) {
     if (!imageUri.isNullOrEmpty()) {
         Glide.with(imageView.context)
             .load(imageUri)
+            .placeholder(R.drawable.icon_logo) // 기본 로딩 이미지 설정
+            .error(R.drawable.icon_logo) // 로딩 에러 시 이미지 설정
             .into(imageView)
     } else {
         imageView.setImageResource(R.drawable.icon_q) // 기본 이미지 설정
