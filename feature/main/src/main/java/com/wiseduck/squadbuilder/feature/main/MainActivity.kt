de46be149a479b82fb8a1883f1c7dde2f05ca1eb
komodgn <a01075251881@gmail.com>
2025-10-26 15:21:31 +0900

    feat: Circuit DI 설정
    - Hilt의 Multibinds를 이용하여 모든 UI 및 Presenter 팩토리를 수집하고 Circuit 인스턴스에 제공
    - 이를 통해 사용자가 Screen을 요청했을 때 적절한 UI 및 Presenter를 자동으로 찾아 생성할 수 있도록 기반 구축

--- feature/main/src/main/java/com/wiseduck/squadbuilder/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/wiseduck/squadbuilder/feature/main/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.slack.circuit.foundation.Circuit
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var circuit: Circuit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
