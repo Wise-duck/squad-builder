12cf5a7a4c969b21a03a13d02da570b309e934a2
komodgn <a01075251881@gmail.com>
2025-10-26 21:45:06 +0900

    feat: 로그인 관련 레포지토리 DI 설정 및 구현 (#5)

--- core/data/impl/src/main/java/com/wiseduck/squadbuilder/core/data/impl/di/RepositoryModule.kt
+++ core/data/impl/src/main/java/com/wiseduck/squadbuilder/core/data/impl/di/RepositoryModule.kt
@@ -1,4 +1,18 @@
 package com.wiseduck.squadbuilder.core.data.impl.di
 
-class RepositoryModule {
+import com.wiseduck.squadbuilder.core.data.api.repository.AuthRepository
+import com.wiseduck.squadbuilder.core.data.impl.repository.AuthRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImpl) : AuthRepository
 }
--- core/network/src/main/java/com/wiseduck/squadbuilder/core/network/service/SquadBuilderService.kt
+++ core/network/src/main/java/com/wiseduck/squadbuilder/core/network/service/SquadBuilderService.kt
@@ -1,20 +1,29 @@
 package com.wiseduck.squadbuilder.core.network.service
 
+import com.wiseduck.squadbuilder.core.network.request.LoginRequest
 import com.wiseduck.squadbuilder.core.network.request.RefreshTokenRequest
+import com.wiseduck.squadbuilder.core.network.response.LoginResponse
 import com.wiseduck.squadbuilder.core.network.response.RefreshTokenResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface SquadBuilderService {
     // PUBLIC ENDPOINTS
     @POST("api/auth/login")
-    suspend fun login()
+    suspend fun login(@Body loginRequest: LoginRequest) : LoginResponse
 
     @POST("api/auth/refresh")
     suspend fun refreshToken(@Body refreshTokenRequest: RefreshTokenRequest) : RefreshTokenResponse
 
     // PRIVATE ENDPOINTS
+    @POST("api/auth/logout")
+    suspend fun logout()
+
+    @DELETE("api/auth/withdraw")
+    suspend fun withdraw()
+
     @GET("api/teams")
     suspend fun getMyTeams()
 }
