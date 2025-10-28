78b589d89d47535b122f351a0ce116d66077fa74
komodgn <a01075251881@gmail.com>
2025-10-28 18:41:01 +0900

    design: 앱 상태 바 색상 변경
    - 시스템 상태 바 아이콘을 밝게 설정
    - 사용하지 않는 라이브러리 제거

--- feature/main/src/main/java/com/wiseduck/squadbuilder/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/wiseduck/squadbuilder/feature/main/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.wiseduck.squadbuilder.feature.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.Circuit
 import com.slack.circuit.foundation.CircuitCompositionLocals
 import com.slack.circuit.foundation.NavigableCircuitContent
 import com.slack.circuit.foundation.rememberCircuitNavigator
 import com.wiseduck.squadbuilder.core.designsystem.theme.SquadBuilderTheme
-import com.wiseduck.squadbuilder.feature.screens.HomeScreen
 import com.wiseduck.squadbuilder.feature.screens.LoginScreen
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -28,6 +30,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val isDarkTheme = isSystemInDarkTheme()
+
+            val systemUiController = rememberSystemUiController()
+
+            LaunchedEffect(isDarkTheme) {
+                systemUiController.setStatusBarColor(
+                    color = Color.Transparent,
+                    darkIcons = false
+                )
+            }
+
             SquadBuilderTheme {
                 val backStack = rememberSaveableBackStack(LoginScreen)
                 val navigator = rememberCircuitNavigator(backStack)
--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.squadbuilder.android.feature)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -9,14 +8,4 @@ android {
 
 ksp {
     arg("circuit.codegen.mode", "hilt")
-}
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
