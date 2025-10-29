199494aad4d92b29d569501a7c8a4940e6aad789
komodgn <a01075251881@gmail.com>
2025-10-29 20:22:03 +0900

    fix: 응답 모델 수정

--- core/network/src/main/java/com/wiseduck/squadbuilder/core/network/service/SquadBuilderService.kt
+++ core/network/src/main/java/com/wiseduck/squadbuilder/core/network/service/SquadBuilderService.kt
@@ -2,29 +2,31 @@ package com.wiseduck.squadbuilder.core.network.service
 
 import com.wiseduck.squadbuilder.core.network.request.LoginRequest
 import com.wiseduck.squadbuilder.core.network.request.RefreshTokenRequest
-import com.wiseduck.squadbuilder.core.network.response.HomeResponse
 import com.wiseduck.squadbuilder.core.network.response.LoginResponse
 import com.wiseduck.squadbuilder.core.network.response.RefreshTokenResponse
+import com.wiseduck.squadbuilder.core.network.response.TeamResponse
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface SquadBuilderService {
-    // PUBLIC ENDPOINTS
+    // PUBLIC ENDPOINTS [AUTH]
     @POST("api/auth/social-login")
     suspend fun login(@Body loginRequest: LoginRequest) : LoginResponse
 
     @POST("api/auth/refresh")
     suspend fun refreshToken(@Body refreshTokenRequest: RefreshTokenRequest) : RefreshTokenResponse
 
-    // PRIVATE ENDPOINTS
+    // PRIVATE ENDPOINTS [AUTH]
     @POST("api/auth/logout")
     suspend fun logout()
 
     @DELETE("api/auth/withdraw")
     suspend fun withdraw()
 
+    // PRIVATE ENDPOINTS [TEAM]
     @GET("api/teams")
-    suspend fun getTeams(): HomeResponse
+    suspend fun getTeams(): List<TeamResponse>
 }
