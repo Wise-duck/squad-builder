803777f6d70699ac4c9c2097c7c74b0ba8465b4e
komodgn <a01075251881@gmail.com>
2025-10-31 22:40:13 +0900

    fea t" 개인정보처리방침 확인 기능 추가
    - 웹뷰 스크린 추가
    - 프로필 화면에 개인정보처리방침 버튼 추가

---
+++ feature/webview/src/main/java/com/wiseduck/squadbuilder/feature/webview/WebViewPresenter.kt
@@ -0,0 +1,42 @@
+package com.wiseduck.squadbuilder.feature.webview
+
+import androidx.compose.runtime.Composable
+import com.slack.circuit.codegen.annotations.CircuitInject
+import com.slack.circuit.runtime.Navigator
+import com.slack.circuit.runtime.presenter.Presenter
+import com.wiseduck.squadbuilder.feature.screens.WebViewScreen
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.components.ActivityRetainedComponent
+
+class WebViewPresenter @AssistedInject constructor(
+    @Assisted private val navigator: Navigator,
+    @Assisted private val screen: WebViewScreen
+) : Presenter<WebViewUiState> {
+
+    @Composable
+    override fun present(): WebViewUiState {
+        fun handleEvent(event: WebViewUiEvent) {
+            when (event) {
+                is WebViewUiEvent.OnBackButtonClick -> {
+                    navigator.pop()
+                }
+            }
+        }
+
+        return WebViewUiState(
+            url = screen.url,
+            eventSink = ::handleEvent
+        )
+    }
+
+    @CircuitInject(WebViewScreen::class, ActivityRetainedComponent::class)
+    @AssistedFactory
+    fun interface Factory {
+        fun create(
+            screen: WebViewScreen,
+            navigator: Navigator
+        ): WebViewPresenter
+    }
+}
