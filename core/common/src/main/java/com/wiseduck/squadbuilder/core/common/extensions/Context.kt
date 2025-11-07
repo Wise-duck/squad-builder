978c306daede779e1e4a673e4a9cb2cd62ce5474
komodgn <a01075251881@gmail.com>
2025-11-07 16:21:37 +0900

    fix: 앱 강제 종료 해결 (#30)
    - 기기에 플레이 스토어 앱이 설치되어 있지 않은 경우, 브라우저로 열도록 처리

--- core/common/src/main/java/com/wiseduck/squadbuilder/core/common/extensions/Context.kt
+++ core/common/src/main/java/com/wiseduck/squadbuilder/core/common/extensions/Context.kt
@@ -2,11 +2,29 @@ package com.wiseduck.squadbuilder.core.common.extensions
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.net.toUri
 import com.wiseduck.squadbuilder.core.common.BuildConfig
 
 fun Context.goToPlayStore() {
-    val intent =
-        Intent(Intent.ACTION_VIEW, "market://details?id=${BuildConfig.PACKAGE_NAME}".toUri())
-    startActivity(intent)
+    val playStoreUri = "market://details?id=${BuildConfig.PACKAGE_NAME}".toUri()
+    val webUri = "https://play.google.com/store/apps/details?id=${BuildConfig.PACKAGE_NAME}".toUri()
+
+    val playStoreIntent = Intent(Intent.ACTION_VIEW, playStoreUri).apply {
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
+
+    try {
+        startActivity(playStoreIntent)
+    } catch (e: Exception) {
+        val webIntent = Intent(Intent.ACTION_VIEW, webUri).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+
+        try {
+            startActivity(webIntent)
+        } catch (webException: Exception) {
+            Log.e("PLAY_STORE_ERROR", "웹 브라우저도 찾을 수 없음")
+        }
+    }
 }
