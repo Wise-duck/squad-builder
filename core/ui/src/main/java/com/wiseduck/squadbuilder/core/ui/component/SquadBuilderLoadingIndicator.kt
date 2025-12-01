55957472b79184663a8021eabc06a595c1201551
komodgn <a01075251881@gmail.com>
2025-12-01 16:59:32 +0900

    design: 로딩 인디케이터 배경색 변경
    - 최상단에 위치하도록 z-index 수정

--- core/ui/src/main/java/com/wiseduck/squadbuilder/core/ui/component/SquadBuilderLoadingIndicator.kt
+++ core/ui/src/main/java/com/wiseduck/squadbuilder/core/ui/component/SquadBuilderLoadingIndicator.kt
@@ -1,12 +1,15 @@
 package com.wiseduck.squadbuilder.core.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.zIndex
 import com.wiseduck.squadbuilder.core.designsystem.ComponentPreview
+import com.wiseduck.squadbuilder.core.designsystem.theme.Black
 import com.wiseduck.squadbuilder.core.designsystem.theme.Neutral500
 import com.wiseduck.squadbuilder.core.designsystem.theme.SquadBuilderTheme
 
@@ -14,7 +17,9 @@ import com.wiseduck.squadbuilder.core.designsystem.theme.SquadBuilderTheme
 fun SquadBuilderLoadingIndicator(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .zIndex(1000f)
+            .background(Black.copy(alpha = 0.6f)),
         contentAlignment = Alignment.Center,
     ) {
         CircularProgressIndicator(
