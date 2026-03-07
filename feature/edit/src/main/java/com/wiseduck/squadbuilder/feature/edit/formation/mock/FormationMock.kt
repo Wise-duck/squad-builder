b559cfecfee3a0d4f220a354a7d6b1754d07cc19
komodgn <a01075251881@gmail.com>
2026-03-08 00:54:59 +0900

    refactor(formation): Extract mock data

--- feature/edit/src/main/java/com/wiseduck/squadbuilder/feature/edit/formation/mock/FormationMock.kt
+++ feature/edit/src/main/java/com/wiseduck/squadbuilder/feature/edit/formation/mock/FormationMock.kt
@@ -5,6 +5,8 @@ import com.wiseduck.squadbuilder.core.model.PlayerQuarterStatusModel
 import com.wiseduck.squadbuilder.feature.edit.formation.FormationUiState
 import com.wiseduck.squadbuilder.feature.edit.formation.data.createDefaultPlayers
 
+val fakeFormationListItem =  FormationListItemModel(1, "4-3-3", "2026-01-01T00:00:00Z")
+
 val fakeFormationList = listOf(
     FormationListItemModel(1, "4-3-3", "2026-01-01T00:00:00Z"),
     FormationListItemModel(2, "4-4-2", "2026-01-02T00:00:00Z"),
