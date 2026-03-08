a089bcb5ad5182c1510fd5f124873b986aef76e6
komodgn <a01075251881@gmail.com>
2026-03-08 19:08:23 +0900

    refactor(player): Extract mock data

--- feature/edit/src/main/java/com/wiseduck/squadbuilder/feature/edit/formation/mock/FormationMock.kt
+++ feature/edit/src/main/java/com/wiseduck/squadbuilder/feature/edit/formation/mock/FormationMock.kt
@@ -2,8 +2,16 @@ package com.wiseduck.squadbuilder.feature.edit.formation.mock
 
 import com.wiseduck.squadbuilder.core.model.FormationListItemModel
 import com.wiseduck.squadbuilder.core.model.PlayerQuarterStatusModel
+import com.wiseduck.squadbuilder.core.model.TeamPlayerModel
 import com.wiseduck.squadbuilder.feature.edit.formation.FormationUiState
 import com.wiseduck.squadbuilder.feature.edit.formation.data.createDefaultPlayers
+
+val fakeTeamPlayers = listOf(
+    TeamPlayerModel(teamId = 1, id = 1, name = "손흥민", backNumber = 7, position = "FW"),
+    TeamPlayerModel(teamId = 1, id = 2, name = "김민재", backNumber = 3, position = "DF"),
+    TeamPlayerModel(teamId = 1, id = 3, name = "이강인", backNumber = 18, position = "MF"),
+    TeamPlayerModel(teamId = 1, id = 4, name = "조현우", backNumber = 1, position = "GK"),
+)
 
 val fakeFormationListItem =  FormationListItemModel(1, "4-3-3", "2026-01-01T00:00:00Z")
 
